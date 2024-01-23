0048d804ead0b5ebb86fcce236e7b472bd22e848
mbsomeday <137542994@qq.com>
2024-01-23 21:16:58 +0000

    a

--- out.py
+++ out.py
@@ -1,10 +1,13 @@
 from data import d1
+import argparse
 
-d1.d1()
+parser = argparse.ArgumentParser(description='argparse testing')
+parser.add_argument('--name', '-n', type=str, default="bk", required=True, help="a programmer's name")
 
-
-
-
+args = parser.parse_args()
+def outer():
+    print('传入的参数:', args.name)
+    
 
 
 
